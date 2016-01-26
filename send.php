55f14ece395d4e6a3723d8b5cf4c1aa8fbfad472
epayinfo <info@epay.info>
2016-01-27 00:34:05 +0330

    Initial release
    Signed-off-by: epayinfo <info@epay.info>

---
+++ send.php
@@ -0,0 +1,18 @@
+<?php
+
+
+$client = new SoapClient('http://api.epay.info/?wsdl');
+
+// Send a normal prize
+$response = $client->send($apicode,$user_wallet,$amount_to_send,1);
+
+// Send a refferal earning
+$response = $client->send($apicode,$user_wallet,$amount_to_send,2,'Referral earnings.');
+
+
+
+/****
+$response['status'] is greater than 0 that means transaction was successful
+***/
+
+?>
