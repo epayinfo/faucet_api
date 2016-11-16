0b6974279766908599359624c60751fc59c5ab83
ePay.info <info@epay.info>
2016-11-17 00:17:01 +0330

    Adding https

--- check_faucet_balance.php
+++ check_faucet_balance.php
@@ -1,9 +1,9 @@
 <?php
 
-$client = new SoapClient('http://api.epay.info/?wsdl');
+$client = new SoapClient('https://api.epay.info/?wsdl');
 
 
-$response = $client->f_balance($apicode,1)
+$response = $client->f_balance($apicode,1);
 
 /***
 Will return faucet's balance in satoshi.
